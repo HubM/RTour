54fc3fcd3f57bd8fe2cd8611383d6f198d8479aa
Hubert Moncenis <hubmoncenis@gmail.com>
2019-01-11 17:47:59 +0100

    :art: improve typos export variables

--- r-app/components/includes/styles/helpers/typos.tsx
+++ r-app/components/includes/styles/helpers/typos.tsx
@@ -1,41 +1,76 @@
+/***
+ * Title
+ */
+
+// Size
 export const titleh1 = {
-  fontFamily: "robotob",
   fontSize: 52,
   lineHeight: 83
 }
 
 export const titleh2 = {
-  fontFamily: "robotom",
   fontSize: 41,
   lineHeight: 67
 }
 
 export const titleh3 = {
-  fontFamily: "robotor",
   fontSize: 32,
   lineHeight: 54
 }
 
 export const titleh4 = {
-  fontFamily: "robotol",
   fontSize: 25,
   lineHeight: 44
 }
 
+// Font Weight
+export const titleLight = {
+  fontFamily: "robotol",
+}
+
+export const titleRegular = {
+  fontFamily: "robotor"
+}
+
+export const titleMedium = {
+  fontFamily: "robotom"
+}
+
+export const titleBold = {
+  fontFamily: "robotob",
+}
+
+
+
+/***
+ * Content
+ */
+
+// Size
 export const contentXs = {
-  fontFamily: "merriwl",
   fontSize: 13,
   lineHeight: 23
 }
 
 export const contentS = {
-  fontFamily: "merriwr",
   fontSize: 16,
   lineHeight: 27
 }
 
 export const contentM = {
-  fontFamily: "merriwb",
   fontSize: 20,
   lineHeight: 33
+}
+
+// FontWeight
+export const contentLight = {
+  fontFamily: "merriwl"
+}
+
+export const contentRegular = {
+  fontFamily: "merriwr"
+}
+
+export const contentBold = {
+  fontFamily: "merriwb"
 }
