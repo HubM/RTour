1e74ea942bd577f7653e114878de0b23364a6ec9
Hubert Moncenis <hubmoncenis@gmail.com>
2019-01-11 17:48:46 +0100

    :art: improve colors helper variables

--- r-app/components/includes/styles/helpers/colors.tsx
+++ r-app/components/includes/styles/helpers/colors.tsx
@@ -1,18 +1,19 @@
-export default {
-  green: {
-    light: "#1D454D",
-    dark: "#0A2328"
-  },
-  yellow: {
-    light: "#FFF784",
-    dark: "#FFEF04"
-  },
-  black: {
-    light: "#2D2D2D",
-    dark: "#1A1A1A"
-  },
-  gray: {
-    light: "#EFEFEF",
-    dark: "#F8F8F8"
-  }
-};
+export const greenColor = {
+  light: "#1D454D",
+  dark: "#0A2328"
+}
+
+export const yellowColor = {
+  light: "#FFF784",
+  dark: "#FFEF04"
+}
+
+export const blackColor = {
+  light: "#2D2D2D",
+  dark: "#1A1A1A"
+}
+
+export const grayColor = {
+  light: "#EFEFEF",
+  dark: "#F8F8F8"
+}
