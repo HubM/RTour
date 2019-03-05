b89620b41957f9c3b26b88346a708bc2691729b7
Hubert Moncenis <hubmoncenis@gmail.com>
2019-03-05 19:51:01 +0100

    🎨 adding Profile route

--- r-app/router/index.tsx
+++ r-app/router/index.tsx
@@ -3,6 +3,7 @@ import { createStackNavigator, createAppContainer } from "react-navigation";
 import RWelcome from "../components/templates/R-Welcome";
 import RRegister from "../components/templates/R-Register";
 import RLogin from "../components/templates/R-Login";
+import RProfile from "../components/templates/R-Profile";
 import RListRoadtrips from "../components/templates/R-ListRoadtrips";
 import RSingleRoadtrip from "../components/templates/R-SingleRoadtrip";
 import RAddARoadtip from "../components/templates/R-AddARoadtrip";
@@ -12,12 +13,13 @@ const RootNavigator = createStackNavigator(
     Welcome: RWelcome,
     Register: RRegister,
     Login: RLogin,
+    Profile: RProfile,
     ListRoadtrips: RListRoadtrips,
     SingleRoadtrip: RSingleRoadtrip,
     AddARoadtrip: RAddARoadtip
   },
   {
-    initialRouteName: "Register"
+    initialRouteName: "Login"
   }
 );
 
