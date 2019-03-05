af28859d97e0a8b2b3cca66f0bbe55723ba967d0
Hubert Moncenis <hubmoncenis@gmail.com>
2019-03-05 19:51:01 +0100

    ✨ create getRoadtrips mongodb call in controller

--- r-api/controllers/roadtrips/index.js
+++ r-api/controllers/roadtrips/index.js
@@ -1,3 +1,15 @@
+const logger = require('../../services/logger');
+
 module.exports.getRoadtrips = (req, res) => {
-  res.send("roadtrips are coming !")
+  global.dbRtour.collection('roadtrips').findOne((errorRoadtrips, roadtrips) => {
+    if (errorRoadtrips) {
+      logger.error('Error on GET roadtrips', errorRoadtrips)
+      res.send("Error with GET Roadtrips request")
+    } else {
+      logger.info(roadtrips)
+      res.send("roadtrips are coming !");
+    }
+  })
+
+
 }
