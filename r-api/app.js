7d4e761ca89f97a57acc4d1350a0402b11560479
Hubert Moncenis <hubmoncenis@gmail.com>
2019-03-05 19:51:01 +0100

    ✨ adding mongodb connection

--- r-api/app.js
+++ r-api/app.js
@@ -1,7 +1,22 @@
 const express = require("express");
-const app = express();
-const api = require("./api");
+const logger = require('./services/logger');
+const { connectDb } = require("./database");
 
+connectDb()
+  .then(() => {
+    logger.info("Database connected");
+  })
+  .catch(err => {
+    logger.error("Database connection fail", err);
+  });
+
+
+const app = express();
+
+
+
+
+const api = require("./api");
 app.use("/api/v1", api);
 
 module.exports = app;
---
+++ r-api/database.js
@@ -0,0 +1,17 @@
+const mongojs = require("mongojs");
+const { mongoConnectionString } = require('./settings');
+
+
+module.exports.connectDb = () =>
+  new Promise((resolve, reject) => {
+    global.dbRtour = mongojs(mongoConnectionString);
+
+    global.dbRtour.on("connect", () => {
+      resolve();
+    });
+
+    global.dbRtour.on("error", err => {
+      reject(err);
+    });
+  });
+
